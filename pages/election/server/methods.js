7c1be934532e103848535c50f7ba4674eed2edd7
Steven <Stchen105@gmail.com>
2017-07-02 20:53:30 -0400

    changes to election page

---
+++ pages/election/server/methods.js
@@ -0,0 +1,13 @@
+Meteor.methods({
+  "getElectionInfo":
+  function(address,electionId){
+    const electionID = 2000; //this is used to look up a specific type of election
+    const ElectionAPIkey = AIzaSyDYoZw_sdVIOmvB1yxnFvdBwNxf9hB7T1M;
+    const url =  "https://www.googleapis.com/civicinfo/v2/voterinfo?key=" + EletionAPIkey + "&address=" +address+"&electionId=" + electionId;
+    //https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyDYoZw_sdVIOmvB1yxnFvdBwNxf9hB7T1M&address=269%20South%20St.%20Waltham%20MA&electionId=2000
+    console.log(url);
+    const z = Meteor.http.call("get", url);
+      return z.content;
+  }
+
+})
