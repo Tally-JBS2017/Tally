17e897bba60b27d4dff4340eb7dd74aa0b84b049
Steven <Stchen105@gmail.com>
2017-07-03 22:10:57 -0400

    election.js

--- pages/election/client/election.js
+++ pages/election/client/election.js
@@ -17,20 +17,23 @@ Template.election.helpers({
 });
 
 Template.election.events({
-  //this is
   "click .getElection": function(event,instance){
+    var xmlhttp = new XMLHttpRequest();
     const address = $(".addressInput").val();
-
-    Meteor.apply("getElectionInfo", [address],{returnStubValue:true},
-      function(error,result){
-        console.dir(["getElectionInfo"],error,result);
-        if(error){
-          console.log("Error!!"+JSON.stringify(error)); return;
-        }
-        console.log(result);
-        t = JSON.parse(result);
-        console.dir(t);
-        return instance.state.set("contest",t.contests);
-      });
+    const electionId = 2000; //this is used to look up a specific type of election
+    const ElectionAPIkey = "AIzaSyDYoZw_sdVIOmvB1yxnFvdBwNxf9hB7T1M";
+    xmlhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+        var url =  "https://www.googleapis.com/civicinfo/v2/voterinfo?key=" + ElectionAPIkey + "&address=" +address+"&electionId=" + electionId;
+        //https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyDYoZw_sdVIOmvB1yxnFvdBwNxf9hB7T1M&address=269%20South%20St.%20Waltham%20MA&electionId=2000
+        var electionInfo = JSON.parse(this.responseText);
+        var type = electionInfo.contest.type.toString();
+        var office = electionInfo.contest.office.toString();
+        console.log(type);
+        console.log(office);
+      }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
   },
 })
