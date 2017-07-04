76c991cda5d86ea33bc70265c2258689ee485cc5
Steven <Stchen105@gmail.com>
2017-07-03 21:05:57 -0400

    elections js page

---
+++ pages/election/client/election.js
@@ -0,0 +1,35 @@
+Template.election.onCreated(function() {
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    contest:[],
+    t:{},
+
+  });
+  console.log("creating the template");
+  console.dir(this.state);
+});
+
+Template.election.helpers({
+  contest: function(){
+    const instance = Template.instance();
+    return instance.state.get("contest");
+  }
+});
+
+Template.election.events({
+  "click .getElection": function(event,instance){
+    const address = $(".addressInput").val();
+
+    Meteor.apply("getElectionInfo", [address],{returnStubValue:true},
+      function(error,result){
+        console.dir(["getElectionInfo"],error,result);
+        if(error){
+          console.log("Error!!"+JSON.stringify(error)); return;
+        }
+        console.log(result);
+        t = JSON.parse(result);
+        console.dir(t);
+        return instance.state.set("contest",t.contests);
+      });
+  },
+})
--- pages/election/server/methods.js
+++ pages/election/server/methods.js
@@ -1,9 +1,9 @@
 Meteor.methods({
   "getElectionInfo":
-  function(address,electionId){
-    const electionID = 2000; //this is used to look up a specific type of election
+  function(address){
+    const electionId = 2000; //this is used to look up a specific type of election
     const ElectionAPIkey = AIzaSyDYoZw_sdVIOmvB1yxnFvdBwNxf9hB7T1M;
-    const url =  "https://www.googleapis.com/civicinfo/v2/voterinfo?key=" + EletionAPIkey + "&address=" +address+"&electionId=" + electionId;
+    const url =  "https://www.googleapis.com/civicinfo/v2/voterinfo?key=" + ElectionAPIkey + "&address=" +address+"&electionId=" + electionId;
     //https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyDYoZw_sdVIOmvB1yxnFvdBwNxf9hB7T1M&address=269%20South%20St.%20Waltham%20MA&electionId=2000
     console.log(url);
     const z = Meteor.http.call("get", url);
