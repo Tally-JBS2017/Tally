9b025cf1bb9c8dfb9033dd4fa9ccf3ef11387924
Elysian1196 <mdowns2015@brandeis.edu>
2017-07-03 21:17:47 -0400

    add comments to profile page

--- pages/profile/client/profile.js
+++ pages/profile/client/profile.js
@@ -2,11 +2,11 @@ Template.profile.helpers({
   current_profile() {return Profiles.find({owner: Meteor.userId()})},
 })
 Template.profile.events({
-  'click #new' : function (e, instance) {
-    var to_be_inserted = {name:'Default', picture: 'images/default_profile.jpg', likes: [], owner: Meteor.userId()};
+  'click #new' : function (e, instance) {//this activates when you press the reset button
+    var to_be_inserted = {name:'Default', owner: Meteor.userId()};
     Meteor.call('profiles.insert', Meteor.userId(), to_be_inserted);
   },
-  'click #name_add' : function (e, instance) {
+  'click #name_add' : function (e, instance) {//this updates the name field
     console.log('changing name')
     const name = instance.$('#name').val();
     instance.$('#name').val("");
