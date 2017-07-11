754f5ec22dfd99a9cebb80c41927cc0b1b7d438d
Steven <Stchen105@gmail.com>
2017-07-11 12:48:39 -0400

    formatted address enter

--- pages/profile/client/profile.js
+++ pages/profile/client/profile.js
@@ -1,6 +1,7 @@
 Template.profile.helpers({
   current_profile() {return Profiles.find({owner: Meteor.userId()})},
 })
+
 Template.profile.events({
   'click #new' : function (e, instance) {//this activates when you press the reset button
     var to_be_inserted = {name:'Default', owner: Meteor.userId()};
@@ -17,5 +18,41 @@ Template.profile.events({
     const address = instance.$('#address').val();
     instance.$('#address').val("");
     Meteor.call('profiles.address.update', Meteor.userId(), address)
+  },
+  'click #state_add' : function (event, instance){
+    console.log('changing state');
+    const state = instance.$('#state').val();
+    instance.$('#state').val("");
+
+    Meteor.call('election.clear');
+    var xmlhttp = new XMLHttpRequest();
+    //setting up the date for today so the api knows what date to start from
+    const d = new Date();
+    const month = d.getMonth()+1;
+    const year = d.getFullYear();
+    const day = d.getDay()+1;
+    const fullDate = month + " " + day + " " + year;
+    const ElectionAPIkey = "aINkNgEHYqnSUX9TT7TEuSQus167GNvHRAdSjLpx";
+
+    var url = "https://api.open.fec.gov/v1/election-dates/?min_election_date=" + fullDate + "&election_state=" + state + "&sort=election_date&page=1&api_key=aINkNgEHYqnSUX9TT7TEuSQus167GNvHRAdSjLpx&per_page=20"
+
+    xmlhttp.onreadystatechange = function(){
+      console.log('here');
+      if(this.readyState == 4 && this.status == 200){
+        var electionInfo = JSON.parse(this.responseText);
+        //var theState = electionInfo.contest[1].district.name.toString();
+        for(i=1; i<electionInfo.results.length; i++){
+          var seat= electionInfo.results[i].election_notes.toString();
+          var date= electionInfo.results[i].election_date.toString();
+          var apistate= electionInfo.results[i].election_state.toString();
+          var type= electionInfo.results[i].election_type_full.toString();
+
+        var information = {seat,date,apistate,type}
+        Meteor.call('election.insert',information);
+        }
+      }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
   }
 })
