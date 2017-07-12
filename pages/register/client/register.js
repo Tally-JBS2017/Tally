dcc70dc5787ff652bf50190dfc762ae9cc36610b
Cindy <macindy96@gmail.com>
2017-07-12 12:31:52 -0400

    added voice for official ID

--- pages/register/client/register.js
+++ pages/register/client/register.js
@@ -2,7 +2,8 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 Template.register.onCreated(function registerOnCreated() {
   this.statepage= new ReactiveVar("");
-});
+  this.recognition= new ReactiveVar("");
+})
 
 Template.register.helpers({
   //this function's purpose is to allow the dynamic template to grab the right template name.
@@ -18,12 +19,11 @@ Template.register.helpers({
       "MA": [{stepname:"What you need to Register online",stepdescrip:"register online you dummy"}],
     };
     return {contentType:page, items:data[page]};
-  }
-
-});
+  },
+})
 
 Template.register.events({
-  'click button'(elt,instance){
+  'click #regisInfo'(elt,instance){
     const zip =instance.$("#zipcode").val();
 
     /*
@@ -49,5 +49,26 @@ Template.register.events({
     This allows blaze to populate the dynamic template with the correct info */
     Template.instance().statepage.set(state);
     //console.log("active variable:"+Template.instance().statepage.get());
-  }
-});
+  },
+  'click #speakButton'(elt,instance){
+    var recognition = new webkitSpeechRecognition();
+     recognition.onresult = function(event){
+       const text = event.results[0][0].transcript;
+       Meteor.call("sendJSONtoAPI_ai", text, { returnStubValue: true }, function(err, result){
+         if(err){
+           window.alert(err);
+           return;
+         }
+
+         console.log(result);
+         console.log(result.data.result.metadata.intentName);
+         //console.log(result.data.result.speech);
+         var msg = new SpeechSynthesisUtterance(result.data.result.speech);
+         window.speechSynthesis.speak(msg);
+       })
+     };
+     recognition.start();
+  },
+
+
+})
